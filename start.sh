0484bf4625b159bbe24b7787c72ae75db3812e74
bozhou.fan <bozhou.fan>
2023-04-06 17:37:50 +0800

    v1.1.0

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-python -m uvicorn performancetest.web.main:app --workers 2 --limit-max-requests 1000 --timeout-keep-alive 15 --limit-concurrency 1000 --proxy-headers --port 80
+python -m uvicorn performancetest.web.main:app --workers 2 --limit-max-requests 1000 --timeout-keep-alive 15 --limit-concurrency 1000 --proxy-headers --port 80 --host 0.0.0.0
+
